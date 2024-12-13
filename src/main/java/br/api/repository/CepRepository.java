a7a0439607552a36cce9b2ce526f3c341cb0e955
Giovanni Bernardo <canaltamojuntooooo@gmail.com>
2024-12-12 21:48:06 -0300

    chore: adicao de novas dependencias
    - adicao de novas dependencias
    - atualizacao da versao do java
    - atualizacao da versao do spring

--- src/main/java/br/api/repository/CepRepository.java
+++ src/main/java/br/api/repository/CepRepository.java
@@ -2,7 +2,7 @@ package br.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import br.api.model.Adress;
+import br.api.entities.Adress;
 
 public interface CepRepository extends JpaRepository<Adress, String> {
     
--- src/main/java/br/api/service/CepService.java
+++ src/main/java/br/api/service/CepService.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import br.api.model.Adress;
+import br.api.entities.Adress;
 
 
 @FeignClient(url= "https://viacep.com.br/ws/" , name = "viacep")
